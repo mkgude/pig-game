8af0f4361b2c9e3e56b1c1fe26a50734db677861
Marian Gude <mkgude@gmail.com>
2020-11-05 13:53:41 -0600

    functionality added to increase score

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 'use strict';
 // selecting elements
+const player0El = document.querySelector(".player--0")
+const player1El = document.querySelector(".player--1")
 const score0El = document.getElementById("score--0")
 const score1El = document.getElementById("score--1")
 const current0El = document.getElementById("current--0")
 const current1El = document.getElementById("current--1")
+
 const diceEl = document.querySelector(".dice")
 const btnNew = document.querySelector(".btn--new")
 const btnRoll = document.querySelector(".btn--roll")
@@ -16,6 +19,16 @@ diceEl.classList.add("hidden")
 const scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
+
+// function to switch players
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active")
+    player1El.classList.toggle("player--active")
+}
+
 // rolling dice functionality
 btnRoll.addEventListener("click", function(){
     // generate a random dice roll
@@ -31,8 +44,20 @@ btnRoll.addEventListener("click", function(){
         document.getElementById(`current--${activePlayer}`).textContent = currentScore
     } else {
         //  if true - switch to next player
-        activePlayer = activePlayer === 0 ? 1 : 0
+        switchPlayer();
     }
+})
 
+btnHold.addEventListener("click", function(){
     
+    // add current score to global score of active player
+    scores[activePlayer] += currentScore
+    console.log(scores[activePlayer])
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+    // check if score is >= 100, if so, finish game
+    // if (scores >= 100){
+
+    // }
+    // else switch to next player
+    switchPlayer();
 })
