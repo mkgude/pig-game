fca95673c7de8990941272cbd965bffe72c9cc18
Marian Gude <mkgude@gmail.com>
2020-11-05 13:33:54 -0600

    created ability to switch from player 1 to player 2 on rolling a 1

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // selecting elements
 const score0El = document.getElementById("score--0")
 const score1El = document.getElementById("score--1")
+const current0El = document.getElementById("current--0")
+const current1El = document.getElementById("current--1")
 const diceEl = document.querySelector(".dice")
 const btnNew = document.querySelector(".btn--new")
 const btnRoll = document.querySelector(".btn--roll")
@@ -11,6 +13,9 @@ score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add("hidden")
 
+const scores = [0, 0]
+let currentScore = 0
+let activePlayer = 0
 // rolling dice functionality
 btnRoll.addEventListener("click", function(){
     // generate a random dice roll
@@ -20,11 +25,14 @@ btnRoll.addEventListener("click", function(){
     diceEl.classList.remove("hidden")
     diceEl.src = `dice-${dice}.png`
     // check for a rolled 1
-    // if (dice !== 1){
-
-    // } else {
-    // //  if true - switch to next player
-    // }
+    if (dice !== 1){
+        // add dice to current score
+        currentScore += dice
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
+    } else {
+        //  if true - switch to next player
+        activePlayer = activePlayer === 0 ? 1 : 0
+    }
 
     
 })
