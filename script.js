56f22970daca42def83d0da09500ba3c82c6a367
Marian Gude <mkgude@gmail.com>
2020-11-05 14:44:56 -0600

    added restart functionality with the 'new game' button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+
 // selecting elements
 const player0El = document.querySelector(".player--0")
 const player1El = document.querySelector(".player--1")
@@ -12,14 +12,27 @@ const btnNew = document.querySelector(".btn--new")
 const btnRoll = document.querySelector(".btn--roll")
 const btnHold = document.querySelector(".btn--hold")
 
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add("hidden")
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0]
-let currentScore = 0
-let activePlayer = 0
-let playing = true
+// init function
+const init = function() {
+    scores = [0, 0]
+    currentScore = 0
+    activePlayer = 0
+    playing = true
+
+    score0El.textContent = 0
+    score1El.textContent = 0
+    current0El.textContent = 0
+    current1El.textContent = 0
+
+    diceEl.classList.add("hidden")
+    player0El.classList.remove("player--winner")
+    player1El.classList.remove("player--winner")
+    player0El.classList.add("player--active")
+    player1El.classList.remove("player--active")
+}
+init()
 
 // function to switch players
 const switchPlayer = function(){
@@ -35,7 +48,6 @@ btnRoll.addEventListener("click", function(){
     if (playing){
         // generate a random dice roll
         const dice = Math.trunc(Math.random()*6) + 1
-        console.log(dice)
         // display dice
         diceEl.classList.remove("hidden")
         diceEl.src = `dice-${dice}.png`
@@ -55,11 +67,11 @@ btnHold.addEventListener("click", function(){
     if (playing){
         // add current score to global score of active player
         scores[activePlayer] += currentScore
-        console.log(scores[activePlayer])
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
         // check if score is >= 100, if so, finish game
-        if (scores[activePlayer] >= 50){
+        if (scores[activePlayer] >= 20){
             playing = false
+            diceEl.classList.add("hidden")
             // finish game
             document.querySelector(`.player--${activePlayer}`).classList.add("player--winner")
             document.querySelector(`.player--${activePlayer}`).classList.remove("player--active")
@@ -70,4 +82,4 @@ btnHold.addEventListener("click", function(){
     }
 })
 
-// btnNew.addEventListener("click", function()) 
+btnNew.addEventListener("click", init) 
