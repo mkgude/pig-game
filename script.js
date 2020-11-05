3b30f82a14d01ac4d8d550cd4af874e14e858b76
Marian Gude <mkgude@gmail.com>
2020-11-05 14:04:42 -0600

    added functionality to not allow buttons to function after game is over

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ diceEl.classList.add("hidden")
 const scores = [0, 0]
 let currentScore = 0
 let activePlayer = 0
+let playing = true
 
 // function to switch players
 const switchPlayer = function(){
@@ -31,35 +32,42 @@ const switchPlayer = function(){
 
 // rolling dice functionality
 btnRoll.addEventListener("click", function(){
-    // generate a random dice roll
-    const dice = Math.trunc(Math.random()*6) + 1
-    console.log(dice)
-    // display dice
-    diceEl.classList.remove("hidden")
-    diceEl.src = `dice-${dice}.png`
-    // check for a rolled 1
-    if (dice !== 1){
-        // add dice to current score
-        currentScore += dice
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore
-    } else {
-        //  if true - switch to next player
-        switchPlayer();
+    if (playing){
+        // generate a random dice roll
+        const dice = Math.trunc(Math.random()*6) + 1
+        console.log(dice)
+        // display dice
+        diceEl.classList.remove("hidden")
+        diceEl.src = `dice-${dice}.png`
+        // check for a rolled 1
+        if (dice !== 1){
+            // add dice to current score
+            currentScore += dice
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore
+        } else {
+            //  if true - switch to next player
+            switchPlayer();
+        }
     }
 })
 
 btnHold.addEventListener("click", function(){
-    
-    // add current score to global score of active player
-    scores[activePlayer] += currentScore
-    console.log(scores[activePlayer])
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
-    // check if score is >= 100, if so, finish game
-    if (scores[activePlayer] >= 50){
-        // finish game
-        document.querySelector(`.player--${activePlayer}`).classList.add("player--winner")
-        document.querySelector(`.player--${activePlayer}`).classList.remove("player--active")
+    if (playing){
+        // add current score to global score of active player
+        scores[activePlayer] += currentScore
+        console.log(scores[activePlayer])
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+        // check if score is >= 100, if so, finish game
+        if (scores[activePlayer] >= 50){
+            playing = false
+            // finish game
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--winner")
+            document.querySelector(`.player--${activePlayer}`).classList.remove("player--active")
+        } else {
+            // else switch to next player
+            switchPlayer();
+        }
     }
-    // else switch to next player
-    switchPlayer();
 })
+
+// btnNew.addEventListener("click", function()) 
